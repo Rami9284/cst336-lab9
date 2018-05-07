46fd0c1187969895fbcb3b52ec8881227db79c1e
Judith Ramirez <judramirez@csumb.edu>
2018-05-07 20:09:38 +0000

    Fixing

--- connect.php
+++ connect.php
@@ -9,13 +9,13 @@ function getDBConnection() {
     $password = "2ecc86ed";
     
     //when connecting from Heroku
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbName = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
-    } 
+    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    //     $host = $url["host"];
+    //     $dbName = substr($url["path"], 1);
+    //     $username = $url["user"];
+    //     $password = $url["pass"];
+    // } 
     
     try {
         //Creates a database connection
