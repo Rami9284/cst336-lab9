3ab25fe30593fe67f90ff87106c02777389549f5
Judith Ramirez <judramirez@csumb.edu>
2018-05-07 20:05:04 +0000

    Adding files

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+
+function getDBConnection() {
+    
+    //C9 db info
+    $host = "us-cdbr-iron-east-05.cleardb.net";
+    $dbName = "heroku_154b829b8c54258";
+    $username = "be784b3cb25ab4";
+    $password = "2ecc86ed";
+    
+    //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+    
+    try {
+        //Creates a database connection
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    
+        // Setting Errorhandling to Exception
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    catch (PDOException $e) {
+       echo "Problems connecting to database!";
+       exit();
+    }
+    
+    
+    return $dbConn;
+}
+
+?>
